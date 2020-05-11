caaa3c8c28828c7cc96d0d3508244d2283148607
Ananya Sreepathi <anisripathi5@gmail.com>
2020-05-10 22:54:42 -0400

    all the validation completed

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ validation = () => {
    const object2 = document.getElementById('primary_area').value;
 
    // avoid numeric regex
-   var patt1 = /[0-9]/g;
+   const patt1 = /[0-9]/g;
 
    //logic
-   var result = patt1.test(obj);
-   var result1 = patt1.test(object);
-   var result2 = patt1.test(object2);;
+   const result = patt1.test(obj);
+   const result1 = patt1.test(object);
+   const result2 = patt1.test(object2);
    if(result | result1 | result2){
-   alert('This field cannot accept numeric values')
+   console.log('This field cannot accept numeric values')
    console.log(result);
    }
     
@@ -27,7 +27,7 @@ validation = () => {
     var output = patt2.test(zip_code);
     var output2 = patt2.test(phone_number);
     if(output | output2){
-        alert('Numbers only')
+        console.log('Numbers only')
         console.log(output);
     }
    
@@ -42,7 +42,7 @@ validation = () => {
     if(email_output){
         console.log(email_output);
     } else {
-       // alert('Incorrect email')
+       // console.log('Incorrect email')
 
     }
 
@@ -54,9 +54,9 @@ validation = () => {
     //logic
     var valid_path = patt4.test(url_path);
     if(valid_path){
-        //alert('correct url')
+        //console.log('correct url')
     }else{
-       // alert('Incorrect url')
+       // console.log('Incorrect url')
 
     }
 
@@ -70,54 +70,211 @@ validation = () => {
         console.log('Please select an option')
         
     }
-    uploadclick();
+    
 }
 
         uploadclick = () => {
 
         // documents validations starts here
+        const input = document.getElementById('shop_license1').click();
+
         const license = document.getElementById('shop_license1').value;
-        const fssai = document.getElementById('fssai1').value;
-        const gstinpan = document.getElementById('gstin_pan1').value;
-
-
-        console.log(license); 
+        console.log(license);
 
         //regex for file type only
-        var patt5 = /(\.pdf|\.jpeg)$/i ;
+        const patt5 = /(\.pdf|\.jpeg)$/i ;
         
         //logic
-        var upload_result1 = patt5.test(license);
-        var upload_result2 = patt5.test(fssai);
-        var upload_result3 = patt5.test(gstinpan);
+        const upload_result1 = patt5.test(license);
 
-        if(upload_result1 | upload_result2 | upload_result3){
-            alert('Uploaded image successfully')
+        if(upload_result1){
+            console.log('Uploaded image successfully')
         }else{
-            alert('Please uplod an image with .pdf or jpeg only')
+            console.log('Please uplod an image with .pdf or jpeg only')
 
         }
 
-        // pictures validations starts here
-        const facade = document.getElementById('facade1').value;
-        const kitchen = document.getElementById('kitchen1').value;
-        const dining = document.getElementById('dining1').value;
-        const locality = document.getElementById('locality1').value;
+        // // pictures validations starts here
+        // const facade = document.getElementById('facade1').value;
+        // const kitchen = document.getElementById('kitchen1').value;
+        // const dining = document.getElementById('dining1').value;
+        // const locality = document.getElementById('locality1').value;
 
-        //regex for images only
-        var patt6 = /(\.png|\.jpeg|\.jpg|\.gif)$/i ;
+        // //regex for images only
+        // const patt6 = /(\.png|\.jpeg|\.jpg|\.gif)$/i ;
         
-        //logic
-        var upload_result4 = patt6.test(facade);
-        var upload_result5 = patt6.test(kitchen);
-        var upload_result6 = patt6.test(dining);
-        var upload_result7 = patt6.test(locality);
+        // //logic
+        // const upload_result4 = patt6.test(facade);
+        // const upload_result5 = patt6.test(kitchen);
+        // const upload_result6 = patt6.test(dining);
+        // const upload_result7 = patt6.test(locality);
 
-        if(upload_result4 | upload_result5| upload_result6 | upload_result7){
-            alert('Uploaded image successfully')
-        }else{
-            alert('Please uplod an image with .png, .jpg, .jpeg, .gif only')
+        // if(upload_result4 | upload_result5| upload_result6 | upload_result7){
+        //     console.log('Uploaded image successfully')
+        // }else{
+        //     console.log('Please uplod an image with .png, .jpg, .jpeg, .gif only')
 
+        // }
         }
+
+        uploadclick_1 = () => {
+            // documents validations starts here
+            const _input = document.getElementById('fssai1').click();
+            debugger;
+            console.log(_input);
+            const fssai = document.getElementById('fssai1').value;
+            console.log(fssai);
+            
+            //regex for file type only
+            const patt_new = /(\.pdf|\.jpeg)$/i ;
+            
+            //logic
+            const upload_result2 = patt_new.test(fssai);
+    
+            if(upload_result2){
+                console.log('Uploaded image successfully')
+            }else{
+                console.log('Please uplod an image with .pdf or jpeg only')   
+            }   
+         }
+
+         uploadclick_2 = () => {
+            // documents validations starts here
+            const _latest = document.getElementById('gstin_pan1').click(); 
+            const gstinpan = document.getElementById('gstin_pan1').value;
+            console.log(gstinpan);
+    
+            //regex for file type only
+            var patt5 = /(\.pdf|\.jpeg)$/i ;
+            
+            //logic
+            const upload_result3 = patt5.test(gstinpan);
+    
+            if(upload_result3){
+                console.log('Uploaded image successfully')
+            }else{
+                console.log('Please uplod an image with .pdf or jpeg only')
+    
+            }
         }
 
+        uploadclick_3 = () => {
+
+            // pictures validations starts here
+            const input_dacade = document.getElementById('facade1').click();
+    
+            const facade = document.getElementById('facade1').value;
+            console.log(facade);
+    
+            //regex for file type only
+             const patt6 = /(\.png|\.jpeg|\.jpg|\.gif)$/i ;
+            
+            //logic
+            const upload_result4 = patt6.test(facade);
+    
+            if(upload_result4){
+                console.log('Uploaded image successfully')
+            }else{
+                console.log('Please uplod an image with .png, .jpg, .jpeg, .gif only')
+    
+            }
+        }
+
+        uploadclick_4 = () => {
+
+            // pictures validations starts here
+            const input_kitchen = document.getElementById('kitchen1').click();
+    
+            const kitchen = document.getElementById('kitchen1').value;
+            console.log(kitchen);
+    
+            //regex for file type only
+             const patt6 = /(\.png|\.jpeg|\.jpg|\.gif)$/i ;
+            
+            //logic
+            const upload_result5 = patt6.test(kitchen);
+    
+            if(upload_result5){
+                console.log('Uploaded image successfully')
+            }else{
+                console.log('Please uplod an image with .png, .jpg, .jpeg, .gif only')
+    
+            }
+        }
+
+        uploadclick_5 = () => {
+
+            // pictures validations starts here
+            const input_dining = document.getElementById('dining1').click();
+    
+            const dining = document.getElementById('dining1').value;
+            console.log(dining);
+    
+            //regex for file type only
+             const patt6 = /(\.png|\.jpeg|\.jpg|\.gif)$/i ;
+            
+            //logic
+            const upload_result6 = patt6.test(dining);
+    
+            if(upload_result6){
+                console.log('Uploaded image successfully')
+            }else{
+                console.log('Please uplod an image with .png, .jpg, .jpeg, .gif only')
+    
+            }
+        }
+        uploadclick_6 = () => {
+
+            // pictures validations starts here
+            const input_locality = document.getElementById('locality1').click();
+    
+            const locality = document.getElementById('locality1').value;            
+            console.log(locality);
+    
+            //regex for file type only
+             const patt6 = /(\.png|\.jpeg|\.jpg|\.gif)$/i ;
+            
+            //logic
+            const upload_result6 = patt6.test(locality);
+    
+            if(upload_result6){
+                console.log('Uploaded image successfully')
+            }else{
+                console.log('Please uplod an image with .png, .jpg, .jpeg, .gif only')
+    
+            }
+        }
+
+        input_updated_value = (e) => {
+            console.log(e.target.value);
+            document.getElementById('file-name').innerHTML = e.target.value;
+        }
+         
+        input_updated_value_a = (e) => {
+            console.log(e.target.value);
+            document.getElementById('file-name1').innerHTML = e.target.value;
+        }
+         
+        input_updated_value_b = (e) => {
+            console.log(e.target.value);
+            document.getElementById('file-name2').innerHTML = e.target.value;
+        }
+        input_updated_value_c = (e) =>{
+            document.getElementById('file-name3').innerHTML = e.target.value;
+   
+        }
+        input_updated_value_d = (e) =>{
+            document.getElementById('file-name4').innerHTML = e.target.value;
+        }
+
+        input_updated_value_e = (e) =>{
+            document.getElementById('file-name5').innerHTML = e.target.value;
+   
+        }
+
+        input_updated_value_f = (e) =>{
+            document.getElementById('file-name6').innerHTML = e.target.value;
+   
+        }
+
+        // study block and inline elements properly
